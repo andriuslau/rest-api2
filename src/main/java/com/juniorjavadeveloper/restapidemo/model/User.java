5891e85d2f0cb1aa107a038e8291f5c214c22a8c
unknown <andriausl@gmail.com>
2021-11-09 22:37:15 +0200

    Added firstName and lastName examples

--- src/main/java/com/juniorjavadeveloper/restapidemo/model/User.java
+++ src/main/java/com/juniorjavadeveloper/restapidemo/model/User.java
@@ -24,23 +24,23 @@ public class User {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    @Schema(maxLength = 255)
+    @Schema(maxLength = 255, example = "John")
     @NotBlank(message = "First name is required and can't be blank.")
     @Size(max = 255, message = "First name can't be more than 255 symbols.")
     private String firstName;
 
-    @Schema()
+    @Schema(maxLength = 255, example = "Doe")
     @NotBlank(message = "Last name is required and can't be blank.")
     @Size(max = 255, message = "Last name can't be more than 255 symbols.")
     private String lastName;
 
-    @Schema(description = "Unique username without spaces", example = "amazinguser")
+    @Schema(minLength = 3, maxLength = 255, description = "Unique username without spaces", example = "amazinguser")
     @NotNull(message = "Username is required.")
     @UserName
     @Column(nullable = false, unique = true)
     private String userName;
 
-    @Schema(description = "Valid email address is required")
+    @Schema(description = "Valid email address is required", example = "johndoe@example.com")
     @NotBlank(message = "Email is required and can't be blank.")
     @Email(message = "Enter valid email.")
     @Column(nullable = false, unique = true)
